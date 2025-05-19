7f97b02975e142046f5d5f6ec924dd8d76f99c3a
Marcelo Dalcin <marcelo_s.dalcin@hotmail.com>
2025-05-19 16:43:06 -0300

    vercel-fix

--- app/components/organisms/footer/Footer.tsx
+++ app/components/organisms/footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
         <>
         <footer className="bg-[var(--color-bg-third)] fixed bottom-0 flex justify-between left-0 w-full p-4 z-50 ">
-            <TextCommon>©2025 Marcelo Dalcin - NextDeadlock</TextCommon>
+            <TextCommon>©2025 Marcelo Dalcin, NextDeadlock</TextCommon>
             <TextCommon>Todos direitos de imagem a Valve</TextCommon>
             <TextCommon>Obrigado Especial a DeadlockAPI</TextCommon>
         </footer>
