ee658963d1551a4027074bd7431d585bae8b13b9
Marcelo Dalcin <marcelo_s.dalcin@hotmail.com>
2025-05-20 11:11:18 -0300

    done

--- app/components/organisms/singeHeroContent/SingleHeroContent.tsx
+++ app/components/organisms/singeHeroContent/SingleHeroContent.tsx
@@ -6,9 +6,12 @@ import { CharacterFromAPI } from "@/app/types/interfaces/interfaces";
 import TextCommon from "../../atoms/textCommon/TextCommon";
 import DadosSingleHero from "../../molecules/dadosSingleHero/DadosSingleHero";
 import DivCardCommon from "../../atoms/divCardCommon/DivCardCommon";
+import { useFavorites } from "@/app/hooks/useFavorites";
 
 export default function SingleHeroContent() {
     const searchParams = useSearchParams();
+    const {toggleFavorite, isFavorite} = useFavorites();
+
     const idParam = searchParams.get('id');
 
     const {data, isLoading, error} = useApi<CharacterFromAPI[]>(`https://assets.deadlock-api.com/v2/heroes`);
@@ -24,6 +27,12 @@ export default function SingleHeroContent() {
     return (
         <>
         <DivCardCommon>
+            <button
+            onClick={() => toggleFavorite((hero.id).toString())}
+            className={`px-2 py-1 rounded ${isFavorite((hero.id).toString()) ? `bg-[var(--color-bg-secundary)]` : `bg-[var(--color-bg-primary)]`}`}
+            >
+                {isFavorite((hero.id).toString()) ? '★' : '☆'}
+            </button>
             <DadosSingleHero id={hero.id} name={hero.name} description={hero.description} images={hero.images}></DadosSingleHero>
         </DivCardCommon>
         </>
--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import GaleriaContent from "../components/organisms/galeriaContent/GaleriaContent";
-import { FavoritesProvider } from "../context/FavoritesContext";
 
 export default function Galeria() {
   return (
